dd74f53495b769eba6d7b47f2afbc5536948a7dc
Nurul Syafini <nurulsyafinisubhan.bintemohamedaidilsubhan@my.jcu.edu.au>
2022-05-12 14:56:28 +0800

    Move price/km from attribute to class attribute

---
+++ taxi.py
@@ -0,0 +1,36 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from car import Car
+
+
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23  # class variable, not a constant
+
+    def __init__(self, name, fuel):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(),
+                                                             self.current_fare_distance,
+                                                             self.price_per_km)
+        # self.price_per_km now refers to the class variable. Used to be an attribute
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        """Drive like parent Car but calculate fare distance as well."""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
